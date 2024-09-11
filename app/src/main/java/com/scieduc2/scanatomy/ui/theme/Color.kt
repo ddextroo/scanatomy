d9935af8f23c0238df851145f0f6c16aec6919fb
ddextroo <dexteringuito@gmail.com>
2024-09-12 06:10:34 +0800

    project structure

--- app/src/main/java/com/scieduc2/scanatomy/ui/theme/Color.kt
+++ app/src/main/java/com/scieduc2/scanatomy/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.scieduc2.scanatomy.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryColor = Color(0xFFb91c1c)
+val SecondaryColor = Color(0xFF8B8D98)
+val LightBackgroundColor = Color(0xFFFEFEFE)
+val DarkBackgroundColor = Color(0xFF111111)
--- app/src/main/java/com/scieduc2/scanatomy/ui/theme/Type.kt
+++ app/src/main/java/com/scieduc2/scanatomy/ui/theme/Type.kt
@@ -2,33 +2,39 @@ package com.scieduc2.scanatomy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.scieduc2.scanatomy.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_reg, FontWeight.Normal),
+    Font(R.font.poppins_med, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.25.sp
     )
-    */
 )
